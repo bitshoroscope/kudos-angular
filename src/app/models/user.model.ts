51800784c1ce03ebb82b395c7cb04dbd72f0d09e
Andy <bitshoroscope@gmail.com>
2021-10-24 23:20:21 -0500

    Added login with firebase!

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,10 @@
 export interface IUser{
     id: number,
     username: string,
+    email:string,
     name: string, 
-    lastname: string
+    lastname: string,
+    photoUrl: string,
+    organization:string
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,27 +1,69 @@
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFireAuth } from "@angular/fire/auth";
 import { IUser } from './../models/user.model';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgZone } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  currentUser:IUser
+  currentUser
 
-  constructor(private router: Router) { }
-
-  login(username: string, password: string){
-    this.currentUser = {
-      id:1,
-      username:'andy',
-      name:'andy',
-      lastname:'bravo'
+  constructor(public afs: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public ngZone: NgZone) { 
+      this.afAuth.authState.subscribe(user => {
+        if (user) {
+          this.currentUser = user;
+          localStorage.setItem('user', JSON.stringify(this.currentUser));
+          JSON.parse(localStorage.getItem('user'));
+        } else {
+          localStorage.setItem('user', null);
+          JSON.parse(localStorage.getItem('user'));
+        }
+      })
     }
-    this.router.navigate(['/give-kudos']);
+
+  login(email: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['give-kudos']);
+        });
+        this.setUserData(result.user);
+      }).catch((error) => {
+        window.alert(error.message)
+      })
   }
 
-  isAuthenticated(){
+  isAuthenticated() {
     return !!this.currentUser;
+  }
+
+  setUserData(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const userData: IUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      photoUrl: user.photoUrl,
+      organization: user.organization,
+      name: user.name,
+      lastname:user.lastname
+    }
+    return userRef.set(userData, {
+      merge: true
+    })
+  }
+
+  signOut() {
+    return this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['sign-in']);
+    })
   }
 }
